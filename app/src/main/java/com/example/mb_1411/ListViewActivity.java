97865814d6291a81ebf5ac46e562ee4c9c35c694
HaVu-lala <dieulien2005@gmail.com>
2025-11-19 04:02:55 +0700

    chinh sua pj

---
+++ app/src/main/java/com/example/mb_1411/ListViewActivity.java
@@ -0,0 +1,109 @@
+package com.example.mb_1411;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class ListViewActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayList<MonHoc> arrayList;
+    private MonHocAdapter adapter;
+    private EditText editText1;
+    private Button btnNhap;
+    private Button btnCapNhat;
+    private int vitri = -1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+
+        // Ánh xạ view và khởi tạo dữ liệu
+        AnhXa();
+
+        // Tạo Adapter
+        adapter = new MonHocAdapter(this, R.layout.row_monhoc, arrayList);
+        listView.setAdapter(adapter);
+
+        // Single item click: show position and load into editText for update
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Toast.makeText(ListViewActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+                editText1.setText(arrayList.get(position).getName());
+                vitri = position;
+            }
+        });
+
+        // Long click: delete item
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Toast.makeText(ListViewActivity.this, "Đã xóa " + arrayList.get(position).getName(), Toast.LENGTH_SHORT).show();
+                arrayList.remove(position);
+                adapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+
+        // Add new item
+        btnNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editText1.getText().toString().trim();
+                if (!name.isEmpty()) {
+                    // Thêm một môn học mới với ảnh mặc định
+                    arrayList.add(new MonHoc(name, "Mô tả cho " + name, R.drawable.ic_launcher_foreground));
+                    adapter.notifyDataSetChanged();
+                    editText1.setText("");
+                } else {
+                    Toast.makeText(ListViewActivity.this, "Enter a value", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // Update selected item
+        btnCapNhat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (vitri >= 0 && vitri < arrayList.size()) {
+                    String newName = editText1.getText().toString().trim();
+                    if (!newName.isEmpty()) {
+                        MonHoc monHoc = arrayList.get(vitri);
+                        monHoc.setName(newName);
+                        adapter.notifyDataSetChanged();
+                        vitri = -1;
+                        editText1.setText("");
+                    } else {
+                        Toast.makeText(ListViewActivity.this, "Enter a value", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(ListViewActivity.this, "Please select an item to update", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void AnhXa() {
+        listView = findViewById(R.id.listview1);
+        editText1 = findViewById(R.id.editText1);
+        btnNhap = findViewById(R.id.btnNhap);
+        btnCapNhat = findViewById(R.id.btnCapNhat);
+        //Thêm dữ liệu vào List
+        arrayList = new ArrayList<>();
+        arrayList.add(new MonHoc("Java","Java 1",R.drawable.java1));
+        arrayList.add(new MonHoc("C#","C# 1",R.drawable.c));
+        arrayList.add(new MonHoc("PHP","PHP 1",R.drawable.php));
+        arrayList.add(new MonHoc("Kotlin","Kotlin 1",R.drawable.kotlin));
+        arrayList.add(new MonHoc("Dart","Dart 1",R.drawable.dart));
+    }
+}
--- app/src/main/java/com/example/mb_1411/MainActivity.java
+++ app/src/main/java/com/example/mb_1411/MainActivity.java
@@ -3,11 +3,7 @@ package com.example.mb_1411;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,18 +11,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
 
-    private ListView listView;
-    private ArrayList<MonHoc> arrayList;
-    private MonHocAdapter adapter;
-    private EditText editText1;
-    private Button btnNhap;
-    private Button btnCapNhat;
+    private Button btnListView;
     private Button btnGridView;
-    private int vitri = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,69 +27,14 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        // Ánh xạ view và khởi tạo dữ liệu
-        AnhXa();
+        btnListView = findViewById(R.id.btnListView);
+        btnGridView = findViewById(R.id.btnGridView);
 
-        // Tạo Adapter
-        adapter = new MonHocAdapter(MainActivity.this, R.layout.row_monhoc, arrayList);
-        //truyền dữ liệu từ adapter ra listview
-        listView.setAdapter(adapter);
-
-        // Single item click: show position and load into editText for update
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        btnListView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT).show();
-                editText1.setText(arrayList.get(position).getName());
-                vitri = position;
-            }
-        });
-
-        // Long click: delete item
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long l) {
-                Toast.makeText(MainActivity.this, "Đã xóa " + arrayList.get(position).getName(), Toast.LENGTH_SHORT).show();
-                arrayList.remove(position);
-                adapter.notifyDataSetChanged();
-                return true;
-            }
-        });
-
-        // Add new item
-        btnNhap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String name = editText1.getText().toString().trim();
-                if (!name.isEmpty()) {
-                    // Thêm một môn học mới với ảnh mặc định
-                    arrayList.add(new MonHoc(name, "Mô tả cho " + name, R.drawable.ic_launcher_foreground));
-                    adapter.notifyDataSetChanged();
-                    editText1.setText("");
-                } else {
-                    Toast.makeText(MainActivity.this, "Enter a value", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        // Update selected item
-        btnCapNhat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (vitri >= 0 && vitri < arrayList.size()) {
-                    String newName = editText1.getText().toString().trim();
-                    if (!newName.isEmpty()) {
-                        MonHoc monHoc = arrayList.get(vitri);
-                        monHoc.setName(newName);
-                        adapter.notifyDataSetChanged();
-                        vitri = -1;
-                        editText1.setText("");
-                    } else {
-                        Toast.makeText(MainActivity.this, "Enter a value", Toast.LENGTH_SHORT).show();
-                    }
-                } else {
-                    Toast.makeText(MainActivity.this, "Please select an item to update", Toast.LENGTH_SHORT).show();
-                }
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListViewActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -112,20 +45,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
-
-    private void AnhXa() {
-        listView = (ListView) findViewById(R.id.listview1);
-        editText1 = (EditText) findViewById(R.id.editText1);
-        btnNhap = (Button) findViewById(R.id.btnNhap);
-        btnCapNhat = (Button) findViewById(R.id.btnCapNhat);
-        btnGridView = (Button) findViewById(R.id.btnGridView);
-        //Thêm dữ liệu vào List
-        arrayList = new ArrayList<>();
-        arrayList.add(new MonHoc("Java","Java 1",R.drawable.java1));
-        arrayList.add(new MonHoc("C#","C# 1",R.drawable.c));
-        arrayList.add(new MonHoc("PHP","PHP 1",R.drawable.php));
-        arrayList.add(new MonHoc("Kotlin","Kotlin 1",R.drawable.kotlin));
-        arrayList.add(new MonHoc("Dart","Dart 1",R.drawable.dart));
     }
 }
